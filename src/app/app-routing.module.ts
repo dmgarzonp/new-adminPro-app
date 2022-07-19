2f434ee464a3711588e969e74b03678187cce4d3
David Garzon <david.garzon@akroscorp.com>
2022-07-19 15:25:38 -0500

    Implementacion los Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,42 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
+
+//Modulo personal
+import { PageRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
+
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: PagesComponent,
-    children: [
-      {path: 'dashboard', component: DashboardComponent},
-      {path: 'progress', component: ProgressComponent},
-      {path: 'grafica1', component: Grafica1Component},
-      {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-    ]
 
-  },
+  //path: '/dashboard' PagesRouting
+  //path: '/auth' AutRouting
+  //path: '/medicos' MedicosRouting
+  //path: '/Hospitales' HospitalesRouting
 
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-
-  {path: '**', component: NopagefoundComponent},
-
-
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: '**', component: NopagefoundComponent}
 ]
 
 
 @NgModule({
-  declarations: [],
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    PageRoutingModule,
+    AuthRoutingModule
   ],
   exports: [
     RouterModule
   ]
 })
 export class AppRoutingModule { }
+
